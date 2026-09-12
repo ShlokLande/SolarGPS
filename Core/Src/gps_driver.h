ee9e001a35b01d8ee60dc3bee27ee22e789746cf
Shlok Lande <shloklande@gmail.com>
2026-09-12 13:48:54 -0700

    GPS .h declarations

---
+++ Core/Src/gps_driver.h
@@ -0,0 +1,35 @@
+/******************************************************************************
+* @file    gps.h
+* @brief   Stores global GPS functions
+******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __GPS__H__
+#define __GPS__H__
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdbool.h"
+#include "i2c.h"
+#include "main.h"
+
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c);
+
+void gps_task();
+
+void read_i2c_gps_module(uint8_t* receive_buffer);
+
+HAL_StatusTypeDef gps_config_meas_rate();
+
+/** CAN SENDING FUNCTION DECLARATIONS */
+void CAN_tx_lat_lon_msg(float latitude, float longitude);
+void CAN_tx_alt_geod_msg(float altitude, float geodHeight);
+void CAN_tx_hdop_vdop_msg(float hdop, float vdop);
+void CAN_tx_pdop_speedKmh_msg(float pdop, float speedKmh);
+void CAN_tx_true_magnetic_heading_msg(float trueHeading, float magneticHeading);
+void CAN_tx_sat_count_view_fix_snr_msg(int satelliteCount, int satInView, int fix, int snr);
+void CAN_tx_lon_side_date_msg(char lonSide, char latSide, char date[7], char utcTime[7]);
+
+void CAN_tx_gps_data_msg(GPS* gps_data);
+
+
+#endif /* __GPS__H__ */
